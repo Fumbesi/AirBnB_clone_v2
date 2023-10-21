20b784a2253484a2107055cafddcb6cc9d4fc78c
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-10-21 03:11:15 -0700

    db

--- web_flask/models/engine/db_storage.py
+++ web_flask/models/engine/db_storage.py
@@ -1,5 +1,4 @@
 def close(self):
-    """
-    Calls remove() method on the private session attribute or close() on the class Session.
-    """
-    self.__session.close()
+     """call remove() method on the private session attribute"""
+     self.__session.remove()
+
