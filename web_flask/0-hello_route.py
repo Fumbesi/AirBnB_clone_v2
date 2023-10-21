92d5360db54c36fc31755388704fbcda74a1bac5
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-10-21 02:37:08 -0700

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-This is a simple Flask web application that responds with "Hello HBNB!".
-"""
-
+""" Starts a Flash Web Application """
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    Route that displays "Hello HBNB!".
-    """
-    return "Hello HBNB!"
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
