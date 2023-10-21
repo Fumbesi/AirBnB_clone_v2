f352ff7780bec13507f1c554b302933c1a31bb33
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-10-21 02:39:08 -0700

    1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""
-This is a simple Flask web application with two routes: '/' and '/hbnb'.
-"""
-
+""" Starts a Flash Web Application HBNB"""
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    Route that displays "Hello HBNB!".
-    """
-    return "Hello HBNB!"
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Route that displays "HBNB".
-    """
-    return "HBNB"
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
