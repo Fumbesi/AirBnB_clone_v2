551251ad7f5263ac79bac5b7424d1f8bab546f73
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-10-21 02:40:50 -0700

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
-"""
-This is a simple Flask web application with three routes: '/', '/hbnb', and '/c/<text>'.
-"""
-
+""" Starts a Flash Web Application C is FUN"""
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    Route that displays "Hello HBNB!".
-    """
-    return "Hello HBNB!"
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Route that displays "HBNB".
-    """
-    return "HBNB"
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_with_text(text):
-    """
-    Route that displays "C " followed by the value of the text variable.
-    Replace underscore (_) symbols with a space.
-    """
-    text = text.replace('_', ' ')
-    return "C " + text
+def c_is_fun(text):
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
